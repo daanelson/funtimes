f0fb733e93c736d21cfc5c251955e90194d7bca7
Dan <dan.nelson8@gmail.com>
2015-09-18 11:27:56 -0500

    added ostensible functionality

--- foo.py
+++ foo.py
@@ -1,3 +1,10 @@
+import numpy as np
+import pandas as pd
+
+dframe = pd.DataFrame(np.arange(20))
+
+print dframe
+
 print 'hooray!'
 print 'wahoo!'
 print 'ok'
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
 i made a readme. Hooray!
+What a great readme. 
